4167bf68eca36caf8bf6ebcb97a591626be74f2f
Peter Odding <peter@peterodding.com>
2024-08-28 13:56:20 +0200

    Fix broken static file references (switch from HTTP to HTTPS)

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://developer.paylogic.com'
+SITEURL = 'https://developer.paylogic.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
